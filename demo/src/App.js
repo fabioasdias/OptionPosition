4605432b24aa5e01a0a9da1c228e3684e20ce7e7
Fabio Dias <fabio.dias@gmail.com>
2018-07-21 19:10:03 -0400

    better json

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Map from './glmap.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Map 
+          URL={"./demo.geojson"}
+        />
       </div>
     );
   }
